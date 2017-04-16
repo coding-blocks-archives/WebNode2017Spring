f52374e1c4ae1d8cdede6b3f5cc3be4478343a04
Arnav Gupta <arnav@codingblocks.com>
2017-04-16 15:34:20 +0530

    middlewares after and before
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture16/ExpressRouters/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+
+function mod1(req, res, next) {
+    console.log('Mod1 running');
+
+    req.query.q = req.query.q.replace('a', 'b');
+
+    next();
+}
+
+function mod2(req, res, next) {
+    console.log('Mod2 running');
+    //next();
+    res.send(res.body.split(' '));
+}
+
+app.use(mod1);
+
+app.get('/', function (req, res, next) {
+    console.log('Get function');
+
+    res.set("a", " Some String Here ");
+    res.body = " You said this to me = " + req.query.q;
+    next();
+
+});
+
+app.use(mod2);
+
+
+app.listen(4444, () => {console.log("http://localhost:4444");})
