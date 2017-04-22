4ae6c96474b2c3846113ace73e5446438047e52a
Arnav Gupta <arnav@codingblocks.com>
2017-04-22 17:39:21 +0530

    finish up client side
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture17/MoreWithDatabases/public_static/js/client.js
+++ Lecture17/MoreWithDatabases/public_static/js/client.js
@@ -5,7 +5,31 @@
 $(function () {
 
     $('#newproductform').submit(function (ev) {
-        console.log(ev);
+        ev.preventDefault();
+        $.post('/api/products/add', {
+            name: $('#productname').val(),
+            price: $('#productprice').val()
+        }, function (data) {
+            console.log(data);
+            let prodDom = "<tr><th> ID </th><th> Product </th><th> Price </th></tr>";
+            for (product of data) {
+                prodDom +=
+                    "<tr>" +
+                    "<td>" + product.id + "</td>" +
+                    "<td>" + product.name + "</td>" +
+                    "<td>" + product.price + "</td>" +
+                    "</tr>"
+            }
+            $('#productlist').html(prodDom);
+
+
+            // $('#productlist').append(
+            //     "<tr>" +
+            //     "<td>" + data.id + "</td>" +
+            //     "<td>" + data.name + "</td>" +
+            //     "<td>" + data.price + "</td> </tr>"
+            // )
+        })
     })
 
 
--- Lecture17/MoreWithDatabases/routes/apirouter.js
+++ Lecture17/MoreWithDatabases/routes/apirouter.js
@@ -2,7 +2,29 @@
  * Created by championswimmer on 22/04/17.
  */
 const route = require('express').Router();
+const dbUtils = require('../dbutils');
+
+route.get('/products', function (req, res) {
+    dbUtils.Product.findAll().then(function (products) {
+        res.send(products);
+    })
+});
+
+route.post('/products/add', function (req, res) {
+    let price = parseInt(req.body.price);
+    let name = req.body.name;
+
+    dbUtils.Product.create({
+        name: name,
+        price: price
+    }).then(function (data) {
+        console.log('Created');
+        console.log(data);
+        res.redirect('/api/products');
+        //res.send(data);
+    })
 
 
+});
 
 module.exports = route;
